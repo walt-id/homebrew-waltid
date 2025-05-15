b2cddf1ebd02a9c4eaedeb2b43c51bfc8ef9d07c
alegomes <alegomes@gmail.com>
2025-05-15 10:29:38 -0300

    Fix artifact download url

--- waltid.rb
+++ waltid.rb
@@ -1,7 +1,7 @@
 class Waltid < Formula
   desc "walt.id CLI tool"
   homepage "https://github.com/walt-id/waltid-identity/tree/main/waltid-applications/waltid-cli"
-  url "https://github.com/youruser/yourtool/releases/download/v1.0.0/yourtool-macos.tar.gz"
+  url "https://github.com/walt-id/homebrew-waltid/releases/download/0.0.1/waltid-macos-v0.0.1-alpha.tar.gz"
   sha256 "4e0d4e0c31ca110bd5d0874183eefb96dbf7c90259d259e08a57e21be9379d4f"
   version "0.0.1"
 
